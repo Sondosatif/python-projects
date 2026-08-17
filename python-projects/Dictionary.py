d5edb801879d896d1144163f8d93e91d0fc83e52
sondosatif <sondosatoof@gmail.com>
2026-08-17 16:57:19 +0300

    python-projects

---
+++ python-projects/Dictionary.py
@@ -0,0 +1,20 @@
+book={
+    "title": "Red Queen",
+    "author": "Victoria Aveyard",
+    "year": 2015,
+    "genre": "Fantasy",
+    "pages": 383,
+    "is_many": True,
+    "rating": 4.1,
+}
+print(book["author"])
+
+user_info={
+    'name':'Sondos',
+    'age': 20,
+    'city':'Gaza',
+    'country':'Palestine',
+    'is_student': True,
+    'hobbies':['discovering','learning','traveling'],
+}
+print(user_info['city'])
